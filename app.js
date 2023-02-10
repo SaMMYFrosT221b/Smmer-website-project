ca22a8e6d789112273cf168d8713b9ad6be37a85
Mohd Adil <mohdadil@iitbhilai.ac.in>
2023-02-10 11:53:43 +0530

    Added footer Path and background changes

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express")
-
+const path = require("path")
 const body_parser = require("body-parser")
 const app = express()
-app.use(express.static(__dirname+'\\public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use('/',(req,res) =>{
     res.render('index.ejs',{})
 })
-
-app.listen(3000);
+app.listen(5000);
